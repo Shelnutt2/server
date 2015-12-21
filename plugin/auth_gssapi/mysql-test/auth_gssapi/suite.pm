2f9ef298adb444fc978d29470204ac124f681ab0
Vladislav Vaintroub <vvaintroub@gmail.com>
2015-12-21 23:28:44 +0100

    fix test on unix

--- plugin/auth_gssapi/mysql-test/auth_gssapi/suite.pm
+++ plugin/auth_gssapi/mysql-test/auth_gssapi/suite.pm
@@ -6,20 +6,25 @@ return "No AUTH_GSSAPI plugin" unless $ENV{AUTH_GSSAPI_SO};
 
 return "Not run for embedded server" if $::opt_embedded_server;
 
-if (IS_WIN)
+if ($^O eq "MSWin32")
 {
-  chomp($fullname=`whoami /UPN 2>NUL` || `whoami`);
-  $fullname=~s/\\/\\\\/; # SQL escaping for backslash
+  chomp(my $fullname =`whoami /UPN 2>NUL` || `whoami`);
+  $fullname = ~s/\\/\\\\/; # SQL escaping for backslash
   $ENV{'GSSAPI_FULLNAME'}  = $fullname;
   $ENV{'GSSAPI_SHORTNAME'} = $ENV{'USERNAME'};
 }
-else
+elsif (!$ENV{'GSSAPI_FULLNAME'})
 {
-  if ($ENV{'GSSAPI_FULLNAME'})
+  my $s = `klist |grep 'Default principal: '`;
+  if ($s)
   {
-    # Remove realm part from user name
-    $ENV{'GSSAPI_SHORTNAME'} =~s/@*//;
+    chomp($s);
+    my $fullname = substr($s,19);
+    my $shortname = (split /@/,$fullname) [0];
+    $ENV{'GSSAPI_FULLNAME'} = $fullname;
+    $ENV{'GSSAPI_SHORTNAME'} = $shortname;
   }
+  # TODO: KRB5_KTNAME might need to be set, and principal name, unless it is mariadb/fqdn@REALM
 }
 
 if (!$ENV{'GSSAPI_FULLNAME'}  || !$ENV{'GSSAPI_SHORTNAME'})
